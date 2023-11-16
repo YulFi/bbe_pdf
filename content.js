63560543616a54abc28e2fa45c28a1d843136809
Guillaume Le Seach <guillaume.leseach@oceandatalab.com>
2023-11-16 12:29:15 +0100

    wip

--- content.js
+++ content.js
@@ -1,56 +1,123 @@
+// Globals
 
 var table = document.getElementsByClassName("table table-hover table-striped");
+var thead = table[0].children[0];
+var theadChildren = table[0].children[0].children;
+var tbody = table[0].children[1];
+var tbodyChildren = tbody.children;
 
-var tableArr = [];
+var id;
+var reverse = -1;
 
-for (var i = 0; i < table[0].children[1].children.length; i++)
+// Functions
+
+function sortById0(a, b)
 {
-	tableArr.push(table[0].children[1].children[i]);
+	return reverse * (a.children[id].innerText.localeCompare(b.children[id].innerText));
 }
 
-//table[0].children[1].children[0].remove();
-
-while (table[0].children[1].lastElementChild) 
+function compareDates(a, b)
 {
-    table[0].children[1].removeChild(table[0].children[1].lastElementChild);
-}
-
-for (var i = 0; i < tableArr.length; i++)
-{
-	console.log(tableArr[i].children[4].innerText);
+	let matchesA = a.children[id].innerText.match(/\d+/g);
+	let matchesB = b.children[id].innerText.match(/\d+/g);
 	
-	if (tableArr[i].children[4].innerText == "0")
-	{
-		table[0].children[1].append(tableArr[i]);
-	}
+	let date1 = new Date(matchesA[2], matchesA[1], matchesA[0]);
+	let date2 = new Date(matchesB[2], matchesB[1], matchesB[0]);
+
+	var res;
+	
+	if (date1 < date2)
+		res = -1;
+	else if (date1 > date2)
+		res = 1;
+	else res = 0;
+
+	return reverse * res;
 }
 
+function sortBy(idTmp)
+{
+	let tableArr = [];
 
+	for (let i = 0; i < tbodyChildren.length; i++)
+	{
+	tableArr.push(tbodyChildren[i]);
+	}
 
-//console.log(table.length);
-//console.log(table[0].children[1].children);
+	while (tbody.lastElementChild)
+	{
+	  tbody.removeChild(tbody.lastElementChild);
+	}
 
-//var list = table[0].children[1].children;
-//
-//var sort_by_number_of_downloads = function(a, b) 
-//{
-//	if (a != null && a.children[4] != null && b != null && b.children[4] != null)
-//		return a.children[4].innerText.localeCompare(b.children[4].innerText);
-//	else
-//		return 0;
-//}
-//
-//var itemsArr = [];
-//
-//for (var i in list) {
-//	itemsArr.push(i);
-//	
-//	if (i.children != null && i.children.length > 3)
-//		console.log(i.children[4].innerText);
-//}
-//
-////itemsArr.sort(sort_by_number_of_downloads);
-//
-//for (i = 0; i < itemsArr.length; ++i) {
-//  table[0].children[1].appendChild(itemsArr[i]);
-//}
+	id = idTmp;
+	tableArr.sort(sortById0);
+	reverse *= -1;
+
+	for (let i = 0; i < tableArr.length; i++)
+	{
+		tbody.append(tableArr[i]);
+	}
+
+	return false;
+}
+
+function sortByDate(idTmp)
+{
+	let tableArr = [];
+
+	for (let i = 0; i < tbodyChildren.length; i++)
+	{
+		tableArr.push(tbodyChildren[i]);
+	}
+
+	while (tbody.lastElementChild)
+	{
+		tbody.removeChild(tbody.lastElementChild);
+	}
+	
+	id = idTmp;
+	tableArr.sort(compareDates);
+	reverse *= -1;
+
+	for (let i = 0; i < tableArr.length; i++)
+	{
+		tbody.append(tableArr[i]);
+	}
+
+	return false;
+}
+
+// HTML modifications
+
+// Name
+{
+	let column = theadChildren[0].cells[0];
+	let text = column.innerText;
+	column.innerText = "";
+	let newElem = document.createElement("a");
+	newElem.onclick=function(){sortBy(0);}
+	newElem.innerText = text;
+	column.appendChild(newElem);
+}
+
+// Date update
+{
+	let column = theadChildren[0].cells[2];
+	let text = column.innerText;
+	column.innerText = "";
+	let newElem = document.createElement("a");
+	newElem.onclick=function(){sortByDate(2);}
+	newElem.innerText = text;
+	column.appendChild(newElem);
+}
+
+// Downloads count
+{
+	let column = theadChildren[0].cells[4];
+	let cloud = column.children[0];
+	column.removeChild(column.lastElementChild);
+	let newElem = document.createElement("a");
+	newElem.onclick=function(){sortBy(4);}
+	newElem.appendChild(cloud);
+	column.appendChild(newElem);
+}
